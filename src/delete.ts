44fb9f76b6fbbb20665eb44f1a0fd9d22cf80e81
Ahmdpolash <ahmedpolash732@gmail.com>
2025-01-26 12:53:23 +0600

    learn how to delte single and multiple data

---
+++ src/delete.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  //single delete
+  //   const singleDelete = await prisma.post.delete({
+  //     where: { id: 3},
+  //   });
+
+  //multiple delete
+  const multipleDelete = await prisma.post.deleteMany();
+
+  console.log(multipleDelete);
+};
+
+main();
