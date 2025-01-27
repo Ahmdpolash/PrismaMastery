6354c829564fcab17d9dd36b04ade53b2418786e
Ahmdpolash <ahmedpolash732@gmail.com>
2025-01-27 22:56:24 +0600

    learn aggregate function in prisma

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -1,17 +1,45 @@
 import { PrismaClient } from "@prisma/client";
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const aggregate = async () => {
-    const avgAge = await prisma.user.aggregate({
-        _avg:{
-            age:true
-        }
-    })
+  const avgAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
 
-    console.log(avgAge)
-}
+  const sum = await prisma.user.aggregate({
+    _avg: {
+      age: true,
+    },
+  });
 
+  //find count of age fields
+  const countAge = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
 
-aggregate()
+  // find count of records
+  const countData = await prisma.user.count();
+
+  // find max age
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+    
+  // find max age
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+
+  console.log(minAge);
+};
+
+aggregate();
