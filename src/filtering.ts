a666856d1523d856aec652015872fa53e9979d1d
Ahmdpolash <ahmedpolash732@gmail.com>
2025-01-27 17:42:14 +0600

    Employing endsWith and equals Filters

--- src/filtering.ts
+++ src/filtering.ts
@@ -40,18 +40,44 @@ const filtering = async () => {
       ],
     },
   });
-    
-    
-    const startsWith = await prisma.user.findMany({
-        where: {
-            email: {
-                startsWith: 'p'
-            }
-        }
-    })
-    
-    
-  console.log(startsWith);
+
+  const startsWith = await prisma.user.findMany({
+    where: {
+      email: {
+        // startsWith: 'p'
+        equals: "polash@exampe.com",
+      },
+    },
+  });
+
+  const userNameArray = ["polash ahmed", "polash ahmed 2"];
+
+  const userNamesByArray = await prisma.user.findMany({
+    where: {
+      username: {
+        in: userNameArray,
+      },
+    },
+  });
+
+  const inDepthData = await prisma.user.findUnique({
+    where: {
+      id: 3,
+      },
+      include: {
+          post: {
+              include: {
+                  postCategory: {
+                      include: {
+                          category:true
+                      }
+                  }
+              }
+          }
+      }
+  });
+
+  console.dir(inDepthData,{depth:Infinity});
 };
 
 filtering();
